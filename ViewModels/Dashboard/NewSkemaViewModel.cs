dbdc3f19749c08f41525b37d273f2d1efdb891bf
Thorbjørn Hedegaard <drcamf@gmail.com>
2022-10-05 15:20:36 +0200

    update skema
    Skema updates

--- ViewModels/Dashboard/NewSkemaViewModel.cs
+++ ViewModels/Dashboard/NewSkemaViewModel.cs
@@ -1,8 +1,13 @@
-﻿using SkoleIT.Models;
+﻿
+using Kotlin;
+using Microsoft.Maui;
+using SkoleIT.Models;
 using SkoleIT.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,16 +19,8 @@ namespace SkoleIT.ViewModels.Dashboard
         private List<Skema> skemaCollection = new List<Skema>();
         private readonly StudentSkemaService _studentSkemaService;
         //private string week;
-       // public string Week { get { return week; } }
-       
-        public class Fruit
-        {
-            public string FruitName { get; set; }
-            public string FruitStuff { get; set; }
-        }
-
-       // ObservableCollection<Fruit> fruits = new ObservableCollection<Fruit>();
-       // public ObservableCollection<Fruit> Fruits { get { return fruits; } }
+        // public string Week { get { return week; } }
+      
         ObservableCollection<Skema> skemas = new ObservableCollection<Skema>();
         public ObservableCollection<Skema> Skemas { get { return skemas; } }
 
@@ -32,69 +29,90 @@ namespace SkoleIT.ViewModels.Dashboard
              get { return skemaCollection; }
              set { skemaCollection = value; }
          }*/
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public DateTime MonButton { get; set; }
+        public DateTime TueButton { get; set; }
+        public DateTime WedButton { get; set; }
+        public DateTime ThurButton { get; set; }
+        public DateTime FriButton { get; set; }
+       
+
         public class Week
         {
             public int WeekNumber { get; set; }
         }
 
+        public int SelectedWeek { get { return newWeek.WeekNumber; }  
+            set
+            {
+                if (newWeek.WeekNumber != value)
+                {
+                    newWeek.WeekNumber = value;
+                    // OnPropertyChanged(nameof(SelectedWeekIndex));
+                    SelectedWeekIndex = newWeek.WeekNumber;
+
+                }
+            }
+        }
+      
+        public Week newWeek;
+
+
+        
+        public int _selectedWeekIndex;
+        public int SelectedWeekIndex
+        {
+            get { return _selectedWeekIndex ; }
+            set
+            {
+                if(_selectedWeekIndex != value)
+                {
+                    _selectedWeekIndex = value;
+                     OnPropertyChanged(nameof(SelectedWeekIndex));
+                    SelectedWeekIndex =_selectedWeekIndex;
+                   
+                }
+            }
+        }
+
         ObservableCollection<Week> weeks = new ObservableCollection<Week>();
         public ObservableCollection<Week> Weeks { get { return weeks; } }
-
 
         public NewSkemaViewModel(StudentSkemaService studentSkemaService)
         {
             _studentSkemaService = studentSkemaService;
             ChangeDay = new Command<DateTime>(StartList);
-            WeekSelect = new Command<int>(DataToListView);
-
-
-
-            /*fruits.Add(new Fruit() { FruitName = "Apple", FruitStuff= "You know" });
-            fruits.Add(new Fruit() { FruitName = "Orange", FruitStuff = "You know more" });
-            fruits.Add(new Fruit() { FruitName = "Banana", FruitStuff = "You know less" });
-            fruits.Add(new Fruit() { FruitName = "Grape", FruitStuff = "You don't know" });
-            fruits.Add(new Fruit() { FruitName = "Mango", FruitStuff = "You know what" });*/
-
-
-
+            NewWeekCom = new Command(WeekList);
             DateTime today = DateTime.Today;
+
+            GetWeekDays(today);
+
             StartList(today);
 
-            
             int week = System.Globalization.ISOWeek.GetWeekOfYear(today);
-            weeks.Add(new Week() { WeekNumber = week - 7 });
-            weeks.Add(new Week() { WeekNumber = week - 6 });
-            weeks.Add(new Week() { WeekNumber = week - 5 });
-            weeks.Add(new Week() { WeekNumber = week - 4 });
-            weeks.Add(new Week() { WeekNumber = week - 3 });
-            weeks.Add(new Week() { WeekNumber = week - 2 });
-            weeks.Add(new Week() { WeekNumber = week - 1 });
-            weeks.Add(new Week() { WeekNumber = week });
-            weeks.Add(new Week() { WeekNumber = week +1 });
-            weeks.Add(new Week() { WeekNumber = week + 2 });
-            weeks.Add(new Week() { WeekNumber = week + 3 });
-            weeks.Add(new Week() { WeekNumber = week + 4 });
-            weeks.Add(new Week() { WeekNumber = week + 5 });
-            weeks.Add(new Week() { WeekNumber = week + 6 });
+            for (int i = -14; i < 14; i++)
+            {
+                weeks.Add(new Week() { WeekNumber = week + i });
+            }
         }
 
         public Command<DateTime> ChangeDay { get; set; }
-        public Command<int> WeekSelect { get; set; }
-
+        public Command NewWeekCom { get; set; }
         private void DataToListView(int week)
         {
-            App.Current.MainPage.DisplayAlert("Alert", week.ToString(), "Okay");
+            App.Current.MainPage.DisplayAlert("Alert", _selectedWeekIndex.ToString(), "Okay");
+        }
+        public void WeekList()
+        {
+
         }
 
         private void StartList(DateTime today)
         {
             skemas.Clear();
-            /*fruits.Add(new Fruit() { FruitName = "th", FruitStuff = "mmmmmm" });
-            fruits.Add(new Fruit() { FruitName = "bh", FruitStuff = "hhhhhhh" });
-            fruits.Add(new Fruit() { FruitName = "er", FruitStuff = "etee" });
-            fruits.Add(new Fruit() { FruitName = "Graertpe", FruitStuff = "dfsdafsd" });
-            fruits.Add(new Fruit() { FruitName = "Manererego", FruitStuff = "tersss" });
-            fruits.Add(new Fruit() { FruitName = "Manererego", FruitStuff = "tersss" });*/
+     
             //App.Current.MainPage.DisplayAlert("Alert", week, "Okay");
             int userid = App.UserDetails.UserId;
 
@@ -108,6 +126,93 @@ namespace SkoleIT.ViewModels.Dashboard
                 }
                 skemas.Add(skema);
             }
+        }
+
+
+        private void GetWeekDays(DateTime today)
+        {
+            
+
+            // App.Current.MainPage.DisplayAlert("Alert", today.ToString(), "Okay");
+            switch (today.ToString("dddd"))
+            {
+                case "Monday":
+                    MonButton = today;
+                    TueButton = today.AddDays(1);
+                    WedButton = today.AddDays(2);
+                    ThurButton = today.AddDays(3);
+                    FriButton = today.AddDays(4);
+                    break;
+                case "Tuesday":
+                    MonButton = today.AddDays(-1);
+                    TueButton = today;
+                    WedButton = today.AddDays(1);
+                    ThurButton = today.AddDays(2);
+                    FriButton = today.AddDays(3);
+                    break;
+                case "Wednesday":
+                    MonButton = today.AddDays(-2);
+                    TueButton = today.AddDays(-1);
+                    WedButton = today;
+                    ThurButton = today.AddDays(1);
+                    FriButton = today.AddDays(2);
+                    break;
+                case "Thursday":
+                    MonButton = today.AddDays(-3);
+                    TueButton = today.AddDays(-2);
+                    WedButton = today.AddDays(-1);
+                    ThurButton = today;
+                    FriButton = today.AddDays(1);
+                    break;
+                case "Friday":
+                    MonButton = today.AddDays(-4);
+                    TueButton = today.AddDays(-3);
+                    WedButton = today.AddDays(-2);
+                    ThurButton = today.AddDays(-1);
+                    FriButton = today;
+                    break;
+
+
+
+            }
+        }
+
+        public static DateTime FirstDateOfWeekISO8601(int year, int weekOfYear)
+        {
+            DateTime jan1 = new DateTime(year, 1, 1);
+            int daysOffset = DayOfWeek.Thursday - jan1.DayOfWeek;
+
+            // Use first Thursday in January to get first week of the year as
+            // it will never be in Week 52/53
+            DateTime firstThursday = jan1.AddDays(daysOffset);
+            var cal = CultureInfo.CurrentCulture.Calendar;
+            int firstWeek = cal.GetWeekOfYear(firstThursday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+            var weekNum = weekOfYear;
+            // As we're adding days to a date in Week 1,
+            // we need to subtract 1 in order to get the right date for week #1
+            if (firstWeek == 1)
+            {
+                weekNum -= 1;
+            }
+
+            // Using the first Thursday as starting week ensures that we are starting in the right year
+            // then we add number of weeks multiplied with days
+            var result = firstThursday.AddDays(weekNum * 7);
+
+            // Subtract 3 days from Thursday to get Monday, which is the first weekday in ISO8601
+            return result.AddDays(-3);
+        }
+
+        void SetNewWeek()
+        {
+            App.Current.MainPage.DisplayAlert("Alert", _selectedWeekIndex.ToString(), "Okay");
+            var weeeek = _selectedWeekIndex;
+        }
+
+        protected new virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- Views/Dashboard/NewSkema.xaml.cs
+++ Views/Dashboard/NewSkema.xaml.cs
@@ -1,27 +1,79 @@
+using Java.Lang;
 using SkoleIT.ViewModels.Dashboard;
 using System.Collections.ObjectModel;
+using System.Globalization;
+using static SkoleIT.ViewModels.Dashboard.NewSkemaViewModel;
 
 namespace SkoleIT.Views.Dashboard;
 
 public partial class NewSkema : ContentPage
 {
-   
+    ObservableCollection<Week> weeks = new ObservableCollection<Week>();
+    public ObservableCollection<Week> Weeks { get { return weeks; } }
 
     public NewSkema(NewSkemaViewModel viewModel)
     {
         InitializeComponent();
         this.BindingContext = viewModel;
         DateTime today = DateTime.Now;
-        GetWeekDays(today);
 
+        int week = System.Globalization.ISOWeek.GetWeekOfYear(today);
+        for (int i = -14; i < 14; i++)
+        {
+            weeks.Add(new Week() { WeekNumber = week + i });
+        }
+
+        //week_picker.SelectedIndex = 14;
+
+        //GetWeekDays(today);
     }
 
     private void Mon_Clicked(object sender, EventArgs e)
     {
-       
+       Picker picker = sender as Picker;
+        DateTime today = DateTime.Now;
+      
+       // App.Current.MainPage.DisplayAlert("Alert", number.ToString(), "Okay");
+        var index = picker.SelectedIndex;
+        var number = picker.Items[index];
+        if (index != 0)
+        {
+            
+            DateTime newdate = FirstDateOfWeekISO8601(2022, int.Parse(number));
+            GetWeekDays(newdate);
+            App.Current.MainPage.DisplayAlert("Alert", newdate.ToString(), "Okay");
+        } else
+        {
+            App.Current.MainPage.DisplayAlert("Alert", "Index ikke sat", "Okay");
+        }
     }
 
+    public static DateTime FirstDateOfWeekISO8601(int year, int weekOfYear)
+    {
+        DateTime jan1 = new DateTime(year, 1, 1);
+        int daysOffset = DayOfWeek.Thursday - jan1.DayOfWeek;
 
+        // Use first Thursday in January to get first week of the year as
+        // it will never be in Week 52/53
+        DateTime firstThursday = jan1.AddDays(daysOffset);
+        var cal = CultureInfo.CurrentCulture.Calendar;
+        int firstWeek = cal.GetWeekOfYear(firstThursday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+        var weekNum = weekOfYear;
+        // As we're adding days to a date in Week 1,
+        // we need to subtract 1 in order to get the right date for week #1
+        if (firstWeek == 1)
+        {
+            weekNum -= 1;
+        }
+
+        // Using the first Thursday as starting week ensures that we are starting in the right year
+        // then we add number of weeks multiplied with days
+        var result = firstThursday.AddDays(weekNum * 7);
+
+        // Subtract 3 days from Thursday to get Monday, which is the first weekday in ISO8601
+        return result.AddDays(-3);
+    }
 
 
 
@@ -33,7 +85,7 @@ public partial class NewSkema : ContentPage
         var thuButton = (Button)this.FindByName("Thu");
         var friButton = (Button)this.FindByName("Fri");
 
-
+       // App.Current.MainPage.DisplayAlert("Alert", today.ToString(), "Okay");
         switch (today.ToString("dddd"))
         {
             case "Monday":
@@ -71,9 +123,11 @@ public partial class NewSkema : ContentPage
                 thuButton.CommandParameter = today.AddDays(-1);
                 friButton.CommandParameter = today;
                 break;
-
-
+               
+                
 
         }
     }
+
+    
 }
